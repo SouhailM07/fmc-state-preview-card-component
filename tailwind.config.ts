c27052184552089eefdcea40a17c0e21bc7cb5d4
shadow <lightshadow416@gmail.com>
2024-03-09 21:04:23 +0100

    project core was built

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        PrimaryVeryDarkBlue: "hsl(233, 47%, 7%)",
+        PrimaryDarkDesaturatedBlue: "hsl(244, 38%, 16%)",
+        PrimarySoftViolet: "hsl(277, 64%, 61%)",
+        NeutralWhite: "hsl(0, 0%, 100%)",
+        NeutralTransparentWhite: "hsla(0, 0%, 100%, 0.75)",
+        NeutralTransparentWhite60: "hsla(0, 0%, 100%, 0.6)",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
